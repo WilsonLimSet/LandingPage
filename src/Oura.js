e583d8a31e4f34f02bbeb19c99c1528cc63b27b8
WilsonLimSet <limsetia@usc.edu>
2023-02-09 23:56:58 -0800

    new auth?

--- src/Oura.js
+++ src/Oura.js
@@ -1,17 +1,21 @@
-const axios = require('axios');
+const headers = {
+  'Authorization': 'Bearer ' + process.env.REACT_APP_OURA_PERSONAL_ACCESS_TOKEN,
+  'Accept': 'application/json'
+};
 
-//Sorry Scott
-async function getSleepData() {
-    const apiKey = process.env.REACT_APP_CLIENT_ID;
-    const headers = new Headers({
-      "Authorization": `Bearer ${apiKey}`,
-      "Content-Type": "application/json",
-    });
-  
-    const response = await fetch("https://api.ouraring.com/v2/sleep", { headers });
-    const data = await response.json();
-    //Whole day working on 353 and physics zz
+const start = '2023-02-01T00:00:00.000Z';
+const end = '2023-02-10T23:59:59.999Z';
+
+const url = `https://api.ouraring.com/v2/sleep?start=${start}&end=${end}`;
+
+fetch(url, {headers})
+  .then(response => response.json())
+  .then(data => {
+    // Handle the response data
     console.log(data);
-  }
-  
+  })
+  .catch(error => {
+    // Handle the error
+    console.error(error);
+  });
   export default getSleepData();
