987206340538cb637659c925690dac6a0506f1e8
WilsonLimSet <limsetia@usc.edu>
2023-02-15 13:54:51 -0800

    sleep test

--- api/getSleepData.js
+++ api/getSleepData.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   let date = new Date();
-  date.setDate(date.getDate() - 2);
+  date.setDate(date.getDate() - 5);
 
   console.log(`Querying data for ${date.toISOString().slice(0, 10)}`);
 
