9c083a7815cf76579bd98ec6a9959fe5a6fe6646
WilsonLimSet <limsetia@usc.edu>
2023-02-17 10:32:43 -0800

    hours + min

--- api/getSleepData.js
+++ api/getSleepData.js
@@ -7,17 +7,16 @@ module.exports = async (req, res) => {
   console.log(`Querying data for ${date.toISOString().slice(0, 10)}`);
 
   try {
-    const response = await axios.get(`https://api.ouraring.com/v2/usercollection/daily_sleep?start_date=${date.toISOString().slice(0, 10)}`, {
+    const response = await axios.get(`https://api.ouraring.com/v2/usercollection/sleep?start_date=${date.toISOString().slice(0, 10)}`, {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_OURA_PERSONAL_ACCESS_TOKEN}`,
       },
     });
-
     const responseData = response.data.data;
-    const endData = responseData[0].score;
+    const endData = responseData[0].total_sleep_duration;
     const dateData = responseData[0].day;
 
-    return res.status(200).json({ score: endData, date: dateData });
+    return res.status(200).json({ total_sleep_duration: endData, date: dateData });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ message: 'Failed to retrieve sleep data' });
