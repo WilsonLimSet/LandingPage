5308fa46a51cab8d4889b3104165b3f25081e8df
WilsonLimSet <limsetia@usc.edu>
2023-02-20 01:23:01 -0800

    logo

--- src/OuraRingLogo.js
+++ src/OuraRingLogo.js
@@ -1,16 +1,11 @@
 import React from 'react';
+import Oura from '.Oura.jpg';
 
 const OuraRingLogo = () => (
-    <svg width="20px" x="0px" y="0px" viewBox="0 0 20 20" style={{ enableBackground: "new 0 0 20 20" }}>
-<metadata>
-Created by potrace 1.16, written by Peter Selinger 2001-2019
-</metadata>
-<g transform="translate(0.000000,240.000000) scale(0.100000,-0.100000)" fill="#000000" stroke="none">
-<path d="M0 1200 l0 -1200 1200 0 1200 0 0 1200 0 1200 -1200 0 -1200 0 0 -1200z m1510 690 l0 -70 -310 0 -310 0 0 70 0 70 310 0 310 0 0 -70z m-165 -215 c177 -46 336 -170 415 -325 71 -139 85 -331 36 -475 -36 -104 -81 -177 -154 -250 -115 -114 -247 -173 -408 -182 -177 -11 -330 46 -461 170 -86 82 -131 152 -169 262 -37 109 -39 254 -5 365 66 217 231 375 451 434 73 20 221 20 295 1z"/>
-<path d="M1058 1531 c-151 -49 -268 -161 -318 -306 -91 -265 65 -559 335 -630 253 -65 505 74 591 327 26 76 23 223 -5 302 -55 154 -180 271 -336 312 -71 19 -198 17 -267 -5z"/>
-</g>
-</svg>
-          );
+    <div style={{ width: '100px', height: '100px', borderRadius: '50%', overflow: 'hidden' }}>
+        <img src={Oura} alt="Profile picture" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+    </div>
+);
   
 
 export default OuraRingLogo;
