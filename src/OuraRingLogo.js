751454e6afeca4022e8fb3d0b4464d4653c5dfde
WilsonLimSet <limsetia@usc.edu>
2023-02-20 01:24:24 -0800

    logo

--- src/OuraRingLogo.js
+++ src/OuraRingLogo.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Oura from '.Oura.jpg';
+import Oura from './Oura.jpg';
 
 const OuraRingLogo = () => (
     <div style={{ width: '100px', height: '100px', borderRadius: '50%', overflow: 'hidden' }}>
         <img src={Oura} alt="Profile picture" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
     </div>
 );
-  
 
 export default OuraRingLogo;
 
