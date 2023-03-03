6e67773185b328320b81a6b3e69f01d68ae9cdb7
WilsonLimSet <limsetia@usc.edu>
2023-03-02 16:24:23 -0800

    why is date bugged

--- src/Oura.js
+++ src/Oura.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 
 const main = async () => {
   let date = new Date();
-  date.setDate(date.getDate() - 1);
-  const access_token = process.env.REACT_APP_OURA_PERSONAL_ACCESS_TOKEN;
+  date.setDate(date.getDate() );
+  const access_token ='';
 
   const response = await axios.get(`https://api.ouraring.com/v2/usercollection/daily_sleep?start_date=${date.toISOString().slice(0, 10)}`, {
     headers: {
